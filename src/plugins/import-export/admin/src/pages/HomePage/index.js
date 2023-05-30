0c95c1e534ecd70bc15d74a2f2af22bcd0778de9
Siddiga Gadirova <siddiga.gadirova@technofusion.az>
2023-05-30 16:36:28 +0400

    --some changes, fix problem with split

--- src/plugins/import-export/admin/src/pages/HomePage/index.js
+++ src/plugins/import-export/admin/src/pages/HomePage/index.js
@@ -6,23 +6,29 @@
 
 import React, {memo, useState, useEffect} from 'react';
 // import PropTypes from 'prop-types';
-import {Button, Layout, BaseHeaderLayout, ContentLayout,EmptyStateLayout} from '@strapi/design-system';
+import {Button, Layout, BaseHeaderLayout, ContentLayout, EmptyStateLayout} from '@strapi/design-system';
 import importExportRequests from "../../api/import-export";
-import { Illo } from "../../components/Illo";
-import { JsonDataList } from "../../components/JsonDataList";
-
+import {Illo} from "../../components/Illo";
+// import { JsonDataList } from "../../components/JsonDataList";
+import {JSONInput} from '@strapi/design-system';
 
 
 const HomePage = () => {
-  const [restaurantsData, setRestaurantsData] = useState([]);
+  const [restaurantsData, setRestaurantsData] = useState({restaurantsData: []});
+  const [isClicked, setIsClicked] = useState(false);
+
   const fetchData = async () => {
     const restaurants = await importExportRequests.getAllRestaurants();
+    console.log("restaurants: ", restaurants)
     setRestaurantsData(restaurants);
+    setIsClicked(true);
   }
 
-  useEffect(async () => {
-    await fetchData();
-  }, [])
+  console.log("---restaurantsData: ", restaurantsData)
+
+  // useEffect(async () => {
+  //   await fetchData();
+  // }, [])
 
   async function importRestaurants(data) {
     await importExportRequests.addRestaurants(data);
@@ -40,7 +46,7 @@ const HomePage = () => {
         <div>
           <Button>Import</Button>
 
-          <Button onClick={() => importExportRequests.getAllRestaurants()}>Export</Button>
+          <Button onClick={() => fetchData()}>Export</Button>
         </div>
 
         <ContentLayout>
@@ -50,12 +56,13 @@ const HomePage = () => {
               content="You don't have any restaurants yet..."
             />
           ) : (
-            <>
-              {/*<JsonDataList*/}
-              {/*  restaurantsData={restaurantsData}*/}
-              {/*/>*/}
-            </>
-          )}
+            // {isClicked === true ? (
+            //   <JSONInput value={JSON.stringify(restaurantsData)}/>
+            // ):(
+            //   <JSONInput value={JSON.stringify(restaurantsData)}/>
+            // )}
+            <JSONInput value={JSON.stringify(restaurantsData)}/>
+            )}
         </ContentLayout>
       </Layout>
     </div>
